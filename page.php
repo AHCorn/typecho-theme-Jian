0de0b3b8a83fcc89f8eb5236cc9fd7d5aa8de527
安和 <42889600+AHQAQ@users.noreply.github.com>
2019-10-19 21:32:55 +0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+<?php $this->need('side.php'); ?>
+<div class="main">
+<div class="article-list">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('/css/post.css'); ?>"> 
+  
+<h1 style="
+    margin-bottom: 0px;
+    margin-top: 0px;
+"><?php $this->title() ?></h1>
+<?php $this->content(); ?>
+
+  <?php $this->need('comments.php'); ?>
+</div>
+
+
+
+      
